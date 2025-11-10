b60c6a321307b490cb9df5f6745a048137d21fae
Zehvems <mbakeebiznes@gmail.com>
2025-11-10 20:20:48 +0100

    feat(mini1): complete stage1 scraper

--- src/bots/mini1/scrape.ts
+++ src/bots/mini1/scrape.ts
@@ -1,8 +1,22 @@
 import axios from "axios";
 import * as cheerio from "cheerio";
+import { promises as fs } from "fs";
+import * as path from "path";
+type Book = { title: string; price: number; url: string };
+
 export async function scrape() {
   console.log("scraping...");
   const res = await axios.get("https://books.toscrape.com/");
   const $ = cheerio.load(res.data);
-  return [];
+  const books: Book[] = [];
+  $(".product_pod").each((_, el) => {
+    const url = $(el).find("h3 a").attr("href") ?? "";
+    const title = $(el).find("h3 a").text();
+    const priceText = $(el).find(".price_color").text();
+    const price = parseFloat(priceText.replace("£", ""));
+    const book = { title, price, url };
+    books.push(book);
+    console.log("Book", _ + 1);
+  });
+  return books;
 }
