230cb24253c2a53f271f735f013edd4cfe60b5cb
Zehvems <mbakeebiznes@gmail.com>
2025-11-16 20:49:31 +0100

    feat(mini1): add screenshots pipeline (stage2 core)

--- src/bots/mini1/run.ts
+++ src/bots/mini1/run.ts
@@ -1,6 +1,9 @@
 import { scrape } from "./scrape";
 import { writeJsonSafe } from "../../utils/file";
 import { log } from "../../utils/log";
+import { grabScreenshot } from "./browser";
+import path from "path";
+let safeName = 0;
 
 export async function run() {
   const data = await scrape();
@@ -8,4 +11,16 @@ export async function run() {
   const filepath = await writeJsonSafe("data/out", data);
   console.log(`Saved file to ${filepath}`);
   await log.info(`OK count=${data.length}`);
+
+  for (const book of data) {
+    const timestamp = new Date().toISOString().slice(19, 23).replace(".", "");
+    const outputPath = path.join(
+      "data",
+      "screens",
+      `${(safeName++).toString() + timestamp}.png`
+    );
+    const url = book.url;
+    await grabScreenshot(url, outputPath);
+    console.log(outputPath);
+  }
 }
